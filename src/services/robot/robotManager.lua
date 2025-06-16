2e67c0c69af276027e2dd4246296127130431d9e
奔跑的秋刀鱼 <631603920@qq.com>
2025-06-16 07:11:33 +0000

    实现获取机器人和返回机器人接口

--- src/services/robot/robotManager.lua
+++ src/services/robot/robotManager.lua
@@ -3,16 +3,56 @@ require "skynet.manager"
 local name = "robotManager"
 local config = require "robot.config"
 local robotDatas = {}
+local freeRobots = {}
+local usingRobots = {}
 local CMD = {}
+
+local function getFreeRobotid()
+    if #freeRobots > 0 then
+        return table.remove(freeRobots, 1)
+    end
+    return nil
+end
+
 function CMD.getRobots(gameid, num)
-    
+    if not gameid or not num or gameid == 0 or num <= 0 then
+        return nil
+    end
+
+    local datas = {}
+    local n = 0
+    for i = 1, num do
+        local id = getFreeRobotid()
+        if id and robotDatas[id] then
+            table.insert(datas, robotDatas[id])
+            usingRobots[id] = true
+            n = n + 1
+            if n >= num then
+                break
+            end
+        end
+    end
+
+    return datas
+end
+
+function CMD.returnRobots(ids)
+    for _,id in ipairs(ids) do
+        if usingRobots[id] then
+            usingRobots[id] = nil
+            table.insert(freeRobots, id)
+        end
+    end
 end
 
 function CMD.start()
     local dbSvr = skynet.localname(".dbserver")
     local robots = skynet.call(dbSvr, "lua", "func","getRobots", config.idbegin, config.idend)
     if robots then
-        robotDatas = robots
+        for _,robot in pairs(robots) do
+            robotDatas[robot.userid] = robot
+            table.insert(freeRobots, robot)
+        end
     end
 end
 
