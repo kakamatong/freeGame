845620e6cd1004053c631d4fb763418cdff89a27
奔跑的秋刀鱼 <631603920@qq.com>
2025-06-26 07:08:21 +0000

    feat(daySignIn): 补充签到日志

--- src/services/activity/tools.lua
+++ src/services/activity/tools.lua
@@ -2,6 +2,7 @@ local tools = {}
 local skynet = require "skynet"
 local cjson = require "cjson"
 
+-- 返回结果
 function tools.result(info)
     local msg = {}
     if info then
@@ -10,17 +11,20 @@ function tools.result(info)
     return {code = 1, result = cjson.encode(msg)}
 end
 
+-- 获取dbserver
 function tools.getDB()
     local dbserver = skynet.localname(".dbserver")
 	assert(dbserver, "dbserver not started")
 	return dbserver
 end
 
+-- 调用redis
 function tools.callRedis(func,...)
     local db = tools.getDB()
     return skynet.call(db, "lua", "funcRedis", func, ...)
 end
 
+-- 调用mysql
 function tools.callMysql(func,...)
     local db = tools.getDB()
     return skynet.call(db, "lua", "func", func, ...)
