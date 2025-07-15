f32232bd656f096d1e36eed9664521001eaf0280
奔跑的秋刀鱼 <631603920@qq.com>
2025-07-15 12:04:16 +0000

    refactor(protoloader): 更新协议文件路径到lobby目录
    将c2s.sproto和s2c.sproto协议文件路径修改为proto/lobby/目录下，以更好地组织协议文件结构

--- src/protoloader.lua
+++ src/protoloader.lua
@@ -6,9 +6,9 @@ local log = require "log"
 
 skynet.start(function()
 	-- 注册客户端到服务端协议（1号）
-	sprotoloader.register("proto/c2s.sproto", 1)
+	sprotoloader.register("proto/lobby/c2s.sproto", 1)
 	-- 注册服务端到客户端协议（2号）
-	sprotoloader.register("proto/s2c.sproto", 2)
+	sprotoloader.register("proto/lobby/s2c.sproto", 2)
 	log.info("protoloader start")
 	-- 注意：不要调用skynet.exit()，否则协议模块会被卸载，导致全局协议失效
 end)
