a4e54ccfa2d2c0b38d6072da6feb4744bdbe9117
奔跑的秋刀鱼 <631603920@qq.com>
2025-07-22 01:57:09 +0000

    refactor(游戏管理): 重构游戏管理服务代码以提高可读性和维护性
    - 在setUserStatus中使用数据结构传递参数
    - 优化checkHaveRoom函数并返回房间对象
    - 统一使用checkHaveRoom检查游戏房间存在性
    - 移除未使用的playerEnter函数

--- src/services/games/gameManager.lua
+++ src/services/games/gameManager.lua
@@ -36,14 +36,16 @@ local function loadSproto()
 end
 
 local function checkHaveRoom(gameid, roomid)
+    local games = allGames[gameid]
     if not allGames[gameid] then
         return false
     end
-    if not allGames[gameid][roomid] then
+    local room = games[roomid]
+    if not room then
         return false
     end
 
-    return true
+    return true, room
 end
 
 -- 创建游戏
@@ -63,9 +65,13 @@ end
 
 -- 销毁游戏
 function CMD.destroyGame(gameid, roomid)
-    local game = allGames[gameid][roomid]
+    local b, room = checkHaveRoom(gameid, roomid)
+    if not b then
+        log.error("game not found %s %s", gameid, roomid)
+        return false
+    end
     allGames[gameid][roomid] = nil
-    skynet.send(game, "lua", "stop")
+    skynet.send(room, "lua", "stop")
     return true
 end
 
@@ -76,45 +82,29 @@ end
 
 -- 获取游戏
 function CMD.getGame(gameid, roomid)
-    return allGames[gameid][roomid]
-end
-
--- 玩家进入游戏
-function CMD.playerEnter(gameid, roomid, userData)
-    local game = allGames[gameid][roomid]
-    if not game then
-        log.error("game not found %s %s", gameid, roomid)
-        return false
-    end
-    local ret = skynet.call(game, "lua", "playerEnter", userData)
-    
-    return ret
+    local b,room = checkHaveRoom(gameid, roomid)
+    return room
 end
 
 -- 连接游戏
 function CMD.connectGame(gameid, roomid, userid, client_fd)
     log.info("connectGame %d %d %d %d", gameid, roomid, userid, client_fd)
-    local games = allGames[gameid]
-    if not games then
-        log.error("game not found %s", gameid)
-        return
-    end
-    local game = games[roomid]
-    if not game then
+    local b, room = checkHaveRoom(gameid, roomid)
+    if not b then
         log.error("room not found %s %s", gameid, roomid)
         return
     end
-    return skynet.call(game, "lua", "connectGame", userid, client_fd)
+    return skynet.call(room, "lua", "connectGame", userid, client_fd)
 end
 
 -- 玩家断线
 function CMD.offLine(gameid, roomid, userid)
-    local game = allGames[gameid][roomid]
-    if not game then
+    local b, room = checkHaveRoom(gameid, roomid)
+    if not b then
         log.error("game not found %s %s", gameid, roomid)
         return false
     end
-    skynet.send(game, "lua", "offLine", userid)
+    skynet.send(room, "lua", "offLine", userid)
 end
 
 skynet.start(function()
