5e90510c5007215783bcc292fe95021137f2af1c
奔跑的秋刀鱼 <631603920@qq.com>
2025-07-29 12:47:20 +0000

    refactor: 移除旧的skynet启动和停止脚本及配置文件
    这些脚本和配置文件已被新的部署方案替代，不再需要维护旧版本。

--- stopGame.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-skynet_pid=$(cat skynetGame.pid)
-
-kill -9 $skynet_pid
-
-rm -f skynetGame.pid
--- stopLobby.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-skynet_pid=$(cat skynetLobby.pid)
-
-kill -9 $skynet_pid
-
-rm -f skynetLobby.pid
