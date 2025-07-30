f8f5aeda546bc9c6d8013ce4e7f31d915809e8ca
奔跑的秋刀鱼 <631603920@qq.com>
2025-07-30 09:26:26 +0000

    chore: 更新服务配置和启动脚本以支持多进程
    修改各服务的pid文件命名以区分不同服务进程
    更新run.sh脚本以顺序启动所有服务
    在.gitignore中添加新增的pid文件

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-./skynet/skynet config/config
+./skynet/skynet config/configLogin
+sleep 1
+./skynet/skynet config/configLobby
+sleep 1
+./skynet/skynet config/configGame
+sleep 1
+./skynet/skynet config/configRobot
+sleep 1
+./skynet/skynet config/configGate
+sleep 1
+./skynet/skynet config/configMatch
