86311219a9852ba4bf4f510566c11feb7565cd42
奔跑的秋刀鱼 <631603920@qq.com>
2025-08-04 01:19:28 +0000

    refactor: 优化服务启动脚本并启用守护进程模式
    移除服务启动间的sleep间隔，添加缺失的服务配置
    为所有服务配置文件启用daemon模式以脱离控制台

--- run.sh
+++ run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 ./skynet/skynet config/configLogin
-sleep 1
-./skynet/skynet config/configLobby
-sleep 1
+./skynet/skynet config/configUser
+./skynet/skynet config/configActivity
+./skynet/skynet config/configAuth
 ./skynet/skynet config/configGame
-sleep 1
 ./skynet/skynet config/configRobot
-sleep 1
 ./skynet/skynet config/configGate
-sleep 1
 ./skynet/skynet config/configMatch
