ae8555292eb30beb5c46de87fd1bb4007ea1b633
kakamatong <631603920@qq.com>
2026-05-18 11:17:25 +0800

    feat(mapConfig): 新增多个地图设计方案
    - 在地图配置中添加多个新的地图设计，包含方块布局及参数
    - 每个地图设计定义最大行列数、图标种类数、总时间及结束时间
    - 地图中使用0表示空白，1为生成方块，9为障碍
    - 更新room模块注释，暂时注释掉默认选择最后一个地图设计的代码，方便测试其他地图方案

--- src/services/games/10002/room.lua
+++ src/services/games/10002/room.lua
@@ -457,7 +457,7 @@ function Room:initLogic()
     local designCount = #mapConfig.MAP_DESIGN
     local selectedDesign = mapConfig.MAP_DESIGN[math.random(1, designCount)]
     -- 测试指定使用
-    selectedDesign = mapConfig.MAP_DESIGN[#mapConfig.MAP_DESIGN] 
+    --selectedDesign = mapConfig.MAP_DESIGN[#mapConfig.MAP_DESIGN - 1] 
     
     local ruleData = {
         playerCnt = self.roomInfo.nowPlayerNum,
